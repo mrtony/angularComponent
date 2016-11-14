2faeb3407704f99e60832c53f3b9063a112b5eab
TonyChen <tonychenn@gmail.com>
2016-11-14 21:24:17 +0800

    顯示空的和有值的星號

--- app/bsComponents/rating.component.js
+++ app/bsComponents/rating.component.js
@@ -7,29 +7,29 @@
     module.component("rating", {
         templateUrl: "../app/bsComponents/rating.component.html",
         bindings: {
+            "max": "<",
             "value": "<"
         },
-        transclude: true,
         controllerAs: "model",
         controller: function () {
             var model = this;
             console.log(model.value);
 
-            // var buildEntries = function (value, max) {
-            //     var result = [];
-            //     for (var i = 1; i <= max; i++) {
-            //         result.push(i <= value ? "star" : "star-empty");
-            //     }
-            //     return result;
-            // };
+            var buildEntries = function (value, max) {
+                var result = [];
+                for (var i = 1; i <= max; i++) {
+                    result.push(i <= value ? "glyphicon-star" : "glyphicon-star-empty");
+                }
+                return result;
+            };
 
             model.$onChanges = function (changes) {
-                //model.entries = buildEntries(model.value, model.max);
+                model.entries = buildEntries(model.value, model.max);
             };
 
             model.$onInit = function () {
                 //初始化需要的資料
-                model.entries = new Array(model.value);
+                model.entries = buildEntries(model.value, model.max);
             };
 
             model.$onDestroy = function () {
