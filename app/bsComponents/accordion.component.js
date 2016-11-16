476fbcb7ea26db0b356ac297ee28a44b63f08d4a
TonyChen <tonychenn@gmail.com>
2016-11-16 23:09:03 +0800

    完成bootstrap accordion

--- app/bsComponents/accordion.component.js
+++ app/bsComponents/accordion.component.js
@@ -6,11 +6,46 @@
 
     var accordionController = function() {
         var model = this;
+        var panels = [];
+
+        model.selectPanel = function(panel) {
+            for(var i in panels) {
+                debugger;
+                if (panel === panels[i])
+                    panels[i].turnOn();
+                else
+                    panels[i].turnOff();
+            }
+        }
+
+        model.addPanel = function(panel) {
+            panels.push(panel);
+
+            if (panels.length > 0)
+                panels[0].turnOn();
+        }
     }
 
     var accordionPanelController = function() {
         var model = this;
-        console.log(model.heading);
+        model.selected = false;
+
+        model.$onInit = function() {
+            model.parent.addPanel(model);
+        }
+
+        model.turnOn = function() {
+            model.selected = true;
+        }
+
+        model.turnOff = function() {
+            model.selected = false;
+        }
+
+        model.select = function() {
+            model.parent.selectPanel(model);
+        }
+
     }
 
     module.component("accordion", {
@@ -24,6 +59,9 @@
         templateUrl: "../app/bsComponents/accordion-panel.component.html",
         bindings: {
             "heading": "@"
+        },
+        require: {
+            "parent": "^accordion"
         },
         transclude: true,
         controllerAs: "model",
